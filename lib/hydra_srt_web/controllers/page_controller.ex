2d770b82cb929ff831e8e2ff52cb91eaf1048a1b
abc3 <root@abc3.dev>
2026-06-03 08:44:41 +0200

    fix(static): index.html path handling in serve_index_html

--- lib/hydra_srt_web/controllers/page_controller.ex
+++ lib/hydra_srt_web/controllers/page_controller.ex
@@ -1,8 +1,6 @@
 defmodule HydraSrtWeb.PageController do
   use HydraSrtWeb, :controller
 
-  @index_html Application.app_dir(:hydra_srt, "priv/static/index.html")
-
   def index(conn, %{"path" => ["index.html" | _rest]}) do
     conn
     |> redirect(to: "/")
@@ -14,8 +12,10 @@ defmodule HydraSrtWeb.PageController do
   end
 
   defp serve_index_html(conn) do
+    index_html = Path.join(:code.priv_dir(:hydra_srt), "static/index.html")
+
     conn
     |> put_resp_header("content-type", "text/html; charset=utf-8")
-    |> Plug.Conn.send_file(200, @index_html)
+    |> Plug.Conn.send_file(200, index_html)
   end
 end
